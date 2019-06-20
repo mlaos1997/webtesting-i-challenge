4fac95b782c89f05b74d9339c1aa4ab6f824cee9
Marcelo Laos <mlaos1997@gmail.com>
2019-06-19 22:01:28 -0400

    completed test for repair function

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,20 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('the enchancer', () => {
+
+    describe('the repair function', () => {
+        it('should restore durability to 100', () => {
+            //arrange
+            const longsword = {
+                name: 'longsword',
+                durability: 40,
+                enhancement: 10
+            }
+            //act
+            const result = enhancer.repair(longsword);
+            expect(result.durability).toBe(100);
+            //assert
+        })
+    });
+})
